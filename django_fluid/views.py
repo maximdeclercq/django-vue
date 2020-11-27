03a98dccc115b7c13e2605bc46a1fdb25adce004
Maxim De Clercq <maximdeclercq00@gmail.com>
2020-11-27 12:06:33 +0100

    Fix Vue routing

--- django_fluid/views.py
+++ django_fluid/views.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import re
+from collections import OrderedDict
 from typing import List
 
 from bs4 import BeautifulSoup
@@ -9,40 +10,31 @@ from django.http import HttpRequest
 from django.template.loader import select_template
 from django.templatetags.static import static
 from django.views.generic import TemplateView
-from inflection import dasherize
+from inflection import camelize, dasherize
 
 
 class DjangoVueView(TemplateView):
     """A mixin that customizes rendering of a view to annotate children of blocks with
     it's name and to return a JSON with only the blocks if an AJAX request is made."""
 
-    vue_components: List[DjangoVueComponent] = []
+    vue_components: dict = {}
     vue_data: dict = {}
+    vue_routes: OrderedDict = OrderedDict()
 
     def get_vue_name(self):
-        return dasherize(
+        return camelize(
             re.sub(r"(component|view)$", "", self.__class__.__name__, re.IGNORECASE)
         )
 
-    def get_vue_definition(self, request, template, *args, **kwargs):
+    def get_vue_definition(self, request, template=None, *args, **kwargs) -> str:
         return f"""
-            const app = Vue.createApp({{
+            const {self.get_vue_name()} = {{
               data() {{
                 return {json.dumps(self.get_vue_data())}
               }},
-              template: `{template}`,
+              template: `{template or self.get_vue_template(request)}`,
               delimiters: ["[[", "]]"],
-            }})
-            const router = VueRouter.createRouter({{
-              history: VueRouter.createWebHashHistory(),
-              routes: [
-                {{ path: '/', component: 'blub-button' }},
-                {{ path: '/about', component: 'blub-button' }},
-              ]
-            }})
-            app.use(router)
-            {("".join(c.get_vue_definition(request) for c in self.get_vue_components()))}
-            app.mount("#app")
+            }}
         """
 
     def get_vue_components(self):
@@ -50,6 +42,16 @@ class DjangoVueView(TemplateView):
 
     def get_vue_data(self):
         return self.vue_data
+
+    def get_vue_routes(self):
+        return self.vue_routes
+
+    def get_vue_template(self, request, **kwargs):
+        self.request = request
+        context = self.get_context_data(**kwargs)
+        response = self.render_to_response(context)
+        response.render()
+        return response.content.decode("utf-8")
 
     def get(self, request: HttpRequest, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
@@ -109,20 +111,43 @@ class DjangoVueView(TemplateView):
                 )
             )
 
+        # Extract styles and scripts from body
         body = soup.find("body")
-
         styles = [e.extract() for e in body.find_all("style")]
         scripts = [e.extract() for e in body.find_all("script")]
         body_content = body.renderContents().decode("utf-8")
 
-        body.clear()
-
-        body.append(soup.new_tag("div", id="app"))
-
+        # Construct Vue app
         vue = soup.new_tag("script")
-        vue.string = self.get_vue_definition(request, body_content, *args, **kwargs)
+        definitions = "\n".join(
+            c.get_vue_definition(request)
+            for c in list(self.get_vue_components().values())
+            + list(self.get_vue_routes().values())
+        )
+        components = "\n".join(
+            f'app.component("{k}", {v.get_vue_name()})'
+            for k, v in self.get_vue_components().items()
+        )
+        routes = ",".join(
+            f'{{ path: "{k}", component: {v.get_vue_name()} }}'
+            for k, v in self.get_vue_routes().items()
+        )
+        vue.string = f"""
+            {definitions}
+            const app = Vue.createApp({self.get_vue_definition(request, body_content, *args, **kwargs)})
+            {components}
+            const router = VueRouter.createRouter({{
+              history: VueRouter.createWebHashHistory(),
+              routes: [{routes}]
+            }})
+            app.use(router)
+            app.mount("#app")
+        """
 
+        # Construct new body
+        body.clear()
         body.extend(styles)
+        body.append(soup.new_tag("div", id="app"))
         body.append(vue)
         body.extend(scripts)
 
@@ -130,31 +155,8 @@ class DjangoVueView(TemplateView):
         return response
 
 
-class DjangoVueComponent(DjangoVueView):
+class NativeVueView(DjangoVueView):
     def get_vue_definition(self, request, *args, **kwargs) -> str:
         return f"""
-            app.component("{self.get_vue_name()}", {{
-              data() {{
-                return {json.dumps(self.get_vue_data())}
-              }},
-              template: `{self.get_vue_template(request)}`,
-              delimiters: ["[[", "]]"],
-            }})
-        """
-
-    def get_vue_template(self, request, **kwargs):
-        self.request = request
-        context = self.get_context_data(**kwargs)
-        response = self.render_to_response(context)
-        response.render()
-        return response.content.decode("utf-8")
-
-
-class NativeVueComponent(DjangoVueComponent):
-    def get_vue_definition(self, request, *args, **kwargs) -> str:
-        return f"""
-            app.component(
-              "{self.get_vue_name()}", 
-              Vue.defineAsyncComponent(() => window["vue3-sfc-loader"].loadModule({select_template(self.get_template_names())})),
-            )
+            const {self.get_vue_name()} = Vue.defineAsyncComponent(() => window["vue3-sfc-loader"].loadModule({select_template(self.get_template_names())}))
         """
